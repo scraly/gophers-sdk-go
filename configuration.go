f48506984a20cf3fb7d04db615804f192cfdd2fe
scraly <scraly@gmail.com>
2023-07-16 11:32:53 +0200

    feat: third version of the generated go sdk

--- configuration.go
+++ configuration.go
@@ -86,7 +86,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/0.0.1/go",
+		UserAgent:        "OpenAPI-Generator/0.0.3/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
